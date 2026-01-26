c09a09c7896aff62ed9c2ac436ef671790fe6420
KAMALESH S <selvavinayagam944@gmail.com>
2026-01-26 20:06:45 +0530

    Create simple Vercel API test

--- api/index.js
+++
@@ -1,9 +0,0 @@
-// Ultra simple test function
-module.exports = (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({
-    status: 'working',
-    message: 'Hello from Vercel!',
-    timestamp: new Date().toISOString()
-  }));
-};
