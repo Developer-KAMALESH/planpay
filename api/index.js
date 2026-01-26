454865875841fc2867c85e944e9d6837fd72263b
KAMALESH S <selvavinayagam944@gmail.com>
2026-01-26 19:31:53 +0530

    Test basic serverless function

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
-const path = require('path');
-
-// Import the built server
-module.exports = require(path.join(__dirname, '..', 'dist', 'index.cjs'));
+// Simple test to see if basic function works
+module.exports = async (req, res) => {
+  try {
+    res.status(200).json({ 
+      message: 'Basic function works!', 
+      timestamp: new Date().toISOString(),
+      method: req.method,
+      url: req.url
+    });
+  } catch (error) {
+    res.status(500).json({ 
+      error: 'Function error', 
+      message: error.message 
+    });
+  }
+};
